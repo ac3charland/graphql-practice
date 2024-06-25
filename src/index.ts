c9677fd7eab7cc8c4173c85640e09ed8d4409164
Alex Charland <ac3charland@gmail.com>
2024-06-25 15:33:29 -0500

    Fix cors error

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
-const {ApolloServer} = require('apollo-server')
+const { ApolloServer } = require("apollo-server");
 
 import { GhibliAPI } from "./datasource";
 import { typeDefs, resolvers } from "./resolver";
 
-const port = process.env.PORT || '4000'
+const port = process.env.PORT || "4000";
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
-    typeDefs, 
-    resolvers,
-    dataSources: () => ({
-        ghibliAPI: new GhibliAPI()
-    })
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    ghibliAPI: new GhibliAPI(),
+  }),
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
 });
 
 // The `listen` method launches a web server.
-server.listen({port}).then(({url}: any) => {
-    console.log(`🚀  Server ready at ${url}`);
+server.listen({ port }).then(({ url }: any) => {
+  console.log(`🚀  Server ready at ${url}`);
 });
